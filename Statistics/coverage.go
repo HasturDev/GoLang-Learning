614ad02f06f8e6a58d8314cbe485b50f89fa8276
Ian.mizer <ian.mizer@onscale.com>
2023-10-13 12:51:41 -0400

    added some stuff and pulled it all together in main... I think

---
+++ Statistics/coverage.go
@@ -0,0 +1,56 @@
+package statistics
+
+import (
+	"fmt"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+// coveragePercentage calculates the code coverage percentage of a Go project.
+func coveragePercentage(repoPath string) (float64, error) {
+	cmd := exec.Command("go", "list", "./...")
+	cmd.Dir = repoPath
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return 0, fmt.Errorf("error listing packages: %v", err)
+	}
+
+	packages := strings.Split(string(out), "\n")
+
+	totalCoverage := 0.0
+	coveredPackages := 0
+
+	// Regex to extract coverage percentage
+	re := regexp.MustCompile(`coverage: ([\d.]+)%`)
+
+	for _, pkg := range packages {
+		if strings.TrimSpace(pkg) == "" {
+			continue
+		}
+
+		cmd := exec.Command("go", "test", "-cover", pkg)
+		cmd.Dir = repoPath
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			return 0, fmt.Errorf("error running tests for package %s: %v", pkg, err)
+		}
+
+		// Extract coverage from the output
+		matches := re.FindStringSubmatch(string(out))
+		if len(matches) > 1 {
+			coverageStr := matches[1]
+			coverage, err := strconv.ParseFloat(coverageStr, 64)
+			if err != nil {
+				return 0, fmt.Errorf("error parsing coverage for package %s: %v", pkg, err)
+			}
+			totalCoverage += coverage
+			coveredPackages++
+		}
+	}
+
+	// Calculate average coverage
+	averageCoverage := totalCoverage / float64(coveredPackages)
+	return averageCoverage, nil
+}
--- main/main.go
+++ main/main.go
@@ -1,30 +1,66 @@
-// Go program to illustrate the
-// concept of init() function
-
-// Declaration of the main package
 package main
 
-// Importing package
 import (
 	"fmt"
+	"log"
+	"os"
 
-	statistics "xithulhu.com/Statistics"
+	"github.com/yourusername/yourprojectname/statistics" // Replace with your actual import path
 )
 
-// Multiple init() function
-
-// items that we'll use later for dictionaries or json or somethign
-func init() {
-	fmt.Println("Welcome to init() function")
-}
-
-// this init is going to initialize items that we'll use later in structs
-func init() {
-	fmt.Println("Hello! init() function")
-}
-
-// Main function
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: program <command> [args...]")
+		fmt.Println("Commands: pull, push, coverage, cleanliness")
+		return
+	}
+
+	command := os.Args[1]
+	repoPath := "./" // Default repository path. Adjust as needed or get it as an argument.
+
+	switch command {
+	case "pull":
+		// Call the pull function (assuming it's named PullRepo)
+		// You might need owner and repo name arguments.
+		if len(os.Args) < 4 {
+			fmt.Println("Usage: program pull <owner> <repo_name>")
+			return
+		}
+		owner := os.Args[2]
+		repoName := os.Args[3]
+		err := PullRepo(owner, repoName)
+		if err != nil {
+			log.Fatalf("Error pulling repository: %v", err)
+		}
+
+	case "push":
+		// Call the push function (assuming it's named PushRepo)
+		err := PushRepo(repoPath)
+		if err != nil {
+			log.Fatalf("Error pushing to repository: %v", err)
+		}
+
+	case "coverage":
+		// Call the coverage function
+		coverage, err := statistics.CoveragePercentage(repoPath)
+		if err != nil {
+			log.Fatalf("Error calculating coverage: %v", err)
+		}
+		fmt.Printf("Code coverage: %.2f%%\n", coverage)
+
+	case "cleanliness":
+		// Call the cleanliness function
+		cleanliness, err := statistics.CleanlinessPercentage(repoPath)
+		if err != nil {
+			log.Fatalf("Error estimating code cleanliness: %v", err)
+		}
+		fmt.Printf("Code cleanliness: %.2f%%\n", cleanliness)
+
+	default:
+		fmt.Println("Unknown command:", command)
+	
+
+
 	// func (r *Repository) Log(o *LogOptions) (object.CommitIter, error)
 	fmt.Println("Welcome to main() function")
 	myCode := statistics.Code{FileSomething: "File1", CodeSomething: "Some Code"}
